da254f4e02996932b62d245de3c2d49e994dd194
Mélissa Loeser <melissa@athom.co>
2017-10-09 14:58:35 +0200

    add controller task and view and migration task

--- blog/database/migrations/2017_10_09_122347_create_tasks_table.php
+++ blog/database/migrations/2017_10_09_122347_create_tasks_table.php
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('task', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 150);
             $table->longText('description');
@@ -29,6 +29,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('task');
+        Schema::dropIfExists('tasks');
     }
 }
