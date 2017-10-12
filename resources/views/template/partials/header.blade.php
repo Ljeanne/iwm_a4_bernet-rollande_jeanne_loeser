51bbc47393eb678822bbd31b197d212064b9ae70
Soggard <e.bernet@laposte.net>
2017-10-12 15:56:15 +0200

    Header temp

--- resources/views/template/partials/header.blade.php
+++ resources/views/template/partials/header.blade.php
@@ -1,28 +1,30 @@
 <!doctype html>
 <!--<html lang="{{ app()->getLocale() }}">-->
-<title>Open Pediatrics</title>
-<meta charset="UTF-8">
-<meta name="description" content="">
-<meta name="keywords" content="">
-<meta name="author" content="">
-<link rel="profile" href="#">
+<head>
+    <title>Open Pediatrics</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <link rel="profile" href="#">
 
-<!--Google Font-->
-<link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Dosis:400,700,500%7CNunito:300,400,600' />
-<!-- Mobile specific meta -->
-<meta name=viewport content="width=device-width, initial-scale=1">
-<meta name="format-detection" content="telephone-no">
+    <!--Google Font-->
+    <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Dosis:400,700,500%7CNunito:300,400,600' />
+    <!-- Mobile specific meta -->
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone-no">
 
-<!-- CSS files -->
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
-<link rel="stylesheet" href="{{asset('css/template.css')}}">
-<link rel="stylesheet" href="{{asset('css/templateplugins.css')}}">
-<script href="{{asset('js/app.js')}}"></script>
+    <!-- CSS files -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/template.css')}}">
+    <link rel="stylesheet" href="{{asset('css/templateplugins.css')}}">
+    <script href="{{asset('js/app.js')}}"></script>
 
 </head>
 <body>
+
 <!--preloading-->
-<div>
+<div id="preloader">
     <img class="logo" src="images/logo1.png" alt="" width="119" height="58">
     <div id="status">
         <span></span>
