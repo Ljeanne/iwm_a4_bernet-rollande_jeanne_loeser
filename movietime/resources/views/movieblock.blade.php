886db7590f6093e74a5cd209a36690ecdf72b35f
Soggard <e.bernet@laposte.net>
2017-10-12 10:15:16 +0200

    movieblock temp

--- movietime/resources/views/movieblock.blade.php
+++ movietime/resources/views/movieblock.blade.php
@@ -2,8 +2,8 @@
     {{--
         <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{$movie->poster_path}}" class="img img-responsive" style="width: 100%">
     --}}
-    <div class="movieblock"  style="width: 100%;height: 500px;background-image: url('https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{$movie->poster_path}}')">
-        <div class="movieinfo" style="/*background: red;width: 100%;height: 100%;position: absolute;*/">
+    <div class="movieblock col-md-12" style="width: 100%;height: 300px;background-image: url('https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{$movie->poster_path}}');">
+        <div class="movieinfo row" style="background: rgba(0,0,0,0.7);width: 100%;height: 100%;position: absolute;bottom: 0;color: #FFF;">
             <ul>
                 <li>{{$movie->id}}</li>
                 <li>{{$movie->title}}</li>
@@ -12,3 +12,16 @@
         </div>
     </div>
 </div>
+
+<style>
+    .movieblock {
+        width: 100%;
+        height: 300px;
+        background-image: url('https://image.tmdb.org/t/p/w185_and_h278_bestv2/{{$movie->poster_path}}')";
+    }
+    .movieinfo {
+        width: 100%;
+        height: 100%;
+        background: green;
+    }
+</style>
